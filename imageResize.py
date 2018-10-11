347357cf8d55868d6da8c46cfaf1bf04128b4f19
Gary Chi <gc1753@nyu.edu>
2018-10-11 11:07:40 -0400

    resize as a function

--- imageResize.py
+++ imageResize.py
@@ -2,28 +2,28 @@ import math
 import string
 from PIL import Image, ImageDraw
 
-newW = 240
-newH = 240
-
 inIm = Image.open("input.png")
 #im = inIm.resize((newW, newH), Image.ANTIALIAS)
 
-pic = inIm.load()
+# input image, width, height
+def squarify(inIm, newW=240, newH=240):
+    pic = inIm.load()
 
-inW, inH = inIm.size
-# print(inW, inH)
+    inW, inH = inIm.size
+    # print(inW, inH)
 
-dW = inW/newW
-dH = inH/newH
-# print(dW, dH)
+    dW = inW/newW
+    dH = inH/newH
+    # print(dW, dH)
 
-im = Image.new("RGBA", (newW, newH), (255, 255, 255, 0))
-img = ImageDraw.Draw(im)
+    im = Image.new("RGBA", (newW, newH), (255, 255, 255, 0))
+    img = ImageDraw.Draw(im)
 
-for x in range(0, newW):
-    for y in range(0, newH):
-        pixColor = '%02x%02x%02x%02x' % pic[int(dW * x), int(dH * y)]
-        img.point((x, y), "#"+pixColor)
+    for x in range(0, newW):
+        for y in range(0, newH):
+            pixColor = '%02x%02x%02x%02x' % pic[int(dW * x), int(dH * y)]
+            img.point((x, y), "#"+pixColor)
 
-# im.show()
-im.save("output.png")
+    return im
+
+squarify(inIm).save("output.png")
