5d2a416adb51ccc1fa4077c6fd3a65d8bd23ee58
Gary Chi <gc1753@nyu.edu>
2018-10-13 13:05:45 -0400

    its not working but

--- imageResize.py
+++ imageResize.py
@@ -1,8 +1,11 @@
+import os
 import math
 import string
+from werkzeug.utils import secure_filename
+from flask import Flask, render_template, flash, request, redirect, url_for, send_file, send_from_directory
 from PIL import Image, ImageDraw
 
-inIm = Image.open("input.png")
+#inIm = Image.open("input.png")
 #im = inIm.resize((newW, newH), Image.ANTIALIAS)
 
 # input image, width, height
@@ -26,4 +29,40 @@ def squarify(inIm, newW=240, newH=240):
 
     return im
 
-squarify(inIm).save("output.png")
+#squarify(inIm).save("output.png")
+
+UPLOAD_FOLDER = '/uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+    return '.' in filename and \
+            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+#print(app.config)
+
+@app.route('/', methods = ['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            
+            inIm = Image.open(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            squarify(inIm).save("output.png")
+            return send_file('output.png')
+
+    return send_from_directory(app.config['UPLOAD_FOLDER'], 'upload.html')
+		
+if __name__ == '__main__':
+    app.run()
