429a6654576274077f221edf7eb928e5df2dec67
Gary Chi <gc1753@nyu.edu>
2018-10-14 20:53:16 -0400

    no more manual resize

--- rezise.py
+++ rezise.py
@@ -17,24 +17,25 @@ newW=240
 newH=240
 def squarify(filename):
 	inIm = Image.open(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-	pic = inIm.load()
-	
-	inW, inH = inIm.size
-    # print(inW, inH)
-
-	dW = inW/newW
-	dH = inH/newH
-    # print(dW, dH)
-
-	im = Image.new("RGBA", (newW, newH), (255, 255, 255, 0))
-	img = ImageDraw.Draw(im)
-
-	for x in range(0, newW):
-		for y in range(0, newH):
-			pixColor = '%02x%02x%02x%02x' % pic[int(dW * x), int(dH * y)]
-			img.point((x, y), "#"+pixColor)
-
-	print "finish resizing"
+	im = inIm.resize((newW, newH), Image.ANTIALIAS)
+#	pic = inIm.load()
+#	
+#	inW, inH = inIm.size
+#    # print(inW, inH)
+#
+#	dW = inW/newW
+#	dH = inH/newH
+#    # print(dW, dH)
+#
+#	im = Image.new("RGBA", (newW, newH), (255, 255, 255, 0))
+#	img = ImageDraw.Draw(im)
+#
+#	for x in range(0, newW):
+#		for y in range(0, newH):
+#			pixColor = '%02x%02x%02x%02x' % pic[int(dW * x), int(dH * y)]
+#			img.point((x, y), "#"+pixColor)
+#
+#	print "finish resizing"
 	return im
 
 def allowed_file(filename):
