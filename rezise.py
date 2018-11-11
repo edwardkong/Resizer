10e0f8e5c21ddfceec5411ad75782cb351970a7f
Gary Chi <gc1753@nyu.edu>
2018-11-11 15:52:04 -0500

    register and login

--- rezise.py
+++ rezise.py
@@ -1,14 +1,26 @@
 import os
 import math
 import string
+import hashlib
+import random
+import pymongo
+from pymongo import MongoClient
 from werkzeug.utils import secure_filename
-from flask import Flask, render_template, flash, request, redirect, url_for, send_file, send_from_directory
+from flask import Flask, render_template, flash, request, redirect, url_for, send_file, send_from_directory, session
 from PIL import Image, ImageDraw
+# from flask_pymongo import PyMongo
+
+active = []
+
+client = MongoClient()
+db = client['squarify']
+users = db['Users']
 
 UPLOAD_FOLDER = 'static'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/squarify"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SECRET_KEY'] = "Plaintext_Secret_Key"
 # app.config['APP_ROOT'] = os.path.dirname(os.path.abspath(__file__))
@@ -50,7 +62,7 @@ def loadResult(filename):
 
 @app.route('/uploader', methods=['GET', 'POST'])
 def uploader():
-	if request.method == 'POST':
+	if request.method == 'POST' and session["token"] in active:
 		print "POST Request"
 		print request.files
 		# check if the post request has the file part
@@ -74,15 +86,51 @@ def uploader():
 			print "Upload resize Successful"
 			return redirect(url_for('loadResult',filename="new"+filename))
 	print "GET Request"
-	return redirect(url_for('index'))	
+	return redirect(url_for('upload'))
 
 @app.route("/")
 def index():
+	return render_template("login.html")
+
+@app.route("/upload")
+def upload():
 	return render_template("upload.html")
 
-@app.route("/hello")
-def hello():
-    return "Hello World!"
-	
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+	if request.method == "POST":
+		salt = str(random.randint(1, 1000))
+		checkUname = str(request.form['uname'])
+		formatPass = str(request.form['pword'])
+		fullStr = checkUname + formatPass + salt
+		newhash = hashlib.sha256(fullStr).hexdigest()
+		if(users.find_one({"uname": checkUname}) == None):
+			tmp = {
+				"uname": request.form['uname'],
+				"hash": newhash,
+				"salt": salt
+			}
+			users.insert_one(tmp)
+			return "Successfully register, please go back to login"
+		return "Username already taken, please go back and try again"
+	return redirect(url_for('index'))
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+	checkUname = str(request.form['uname'])
+	formatPass = str(request.form['pword'])
+	toCheck = users.find_one({"uname": checkUname})
+	if request.method == "POST":
+		if(toCheck == None):
+			return "Sorry, login has failed. Please check your credentials."
+		fullStr = checkUname + formatPass + toCheck["salt"]
+		newhash = hashlib.sha256(fullStr).hexdigest()
+		if(newhash == toCheck["hash"]):
+			session['token'] = newhash
+			active.append(newhash)
+			return redirect(url_for('upload'))
+		return "Sorry, login has failed. Please check your credentials."
+	return redirect(url_for('index'))
+
 if __name__ == '__main__':
     app.run(debug=True)
